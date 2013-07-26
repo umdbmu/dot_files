35c7d43faa855e3fc26771ad8dea5e458dbccd3a
umdbmu <umdlbmu@gmail.com>
2013-07-26 14:59:53 +0900

    shell-pop との不具合修正

--- conf/init-shell.el
+++ conf/init-shell.el
@@ -1,14 +1,3 @@
-;; shell-popを読み込み
-(require 'shell-pop)
-;; sh,eshell 以外のシェルを使用する
-(add-to-list 'shell-pop-internal-mode-list '("multi-term" "*terminal<1>*" '(lambda () (multi-term))))
-(shell-pop-set-internal-mode "multi-term")
-;; 出現位置の設定
-(shell-pop-set-window-height 25)
-(shell-pop-set-internal-mode-shell shell-file-name)
-;; shell-popの割り当て
-(global-set-key (kbd "C-c z") 'shell-pop)
-
 ;; 使用するshellを決定
 (defun skt:shell ()
   (or (executable-find "zsh")
@@ -23,3 +12,20 @@
 
 ;; emacsが保持するterminfoを利用する
 (setq system-uses-terminfo nil)
+
+;; shell-popを読み込み
+(require 'shell-pop)
+;; sh,eshell 以外のシェルを使用する
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
+ '(custom-enabled-themes (quote (manoj-dark)))
+ '(custom-safe-themes (quote ("6cfe5b2f818c7b52723f3e121d1157cf9d95ed8923dbc1b47f392da80ef7495d" default)))
+ '(shell-pop-shell-type (quote ("multi-term" "*multi-term*" (lambda nil (multi-term)))))
+ '(shell-pop-universal-key "C-c z")
+ '(shell-pop-window-height 25)
+ '(shell-pop-window-position "bottom")
+ '(yas-trigger-key "TAB"))
--- init.el
+++ init.el
@@ -49,3 +49,22 @@
 ;(require 'one-key-default)
 ;(define-key global-map "\C-x" 'one-key-menu-C-x)
 ;(one-key-default-setup-keys)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
+ '(custom-enabled-themes (quote (manoj-dark)))
+ '(custom-safe-themes (quote ("6cfe5b2f818c7b52723f3e121d1157cf9d95ed8923dbc1b47f392da80ef7495d" default)))
+ '(shell-pop-shell-type (quote ("multi-term" "*multi-term*" (lambda nil (multi-term)))))
+ '(shell-pop-universal-key "C-c z")
+ '(shell-pop-window-height 25)
+ '(shell-pop-window-position "bottom")
+ '(yas-trigger-key "TAB"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
