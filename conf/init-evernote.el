96306280a48894e72185b00fdfe976c3a23a3f2e
umdbmu <umdlbmu@gmail.com>
2013-07-26 15:10:00 +0900

    delete init-evernote

--- conf/init-evernote.el
+++
@@ -1,3 +0,0 @@
-;; (require 'evernote-mode)
-;; (setq evernote-username "urato") ; optional: you can use this username as def
-;; ;(setq evernote-enml-formatter-command '("w3m" "-dump" "-I" "UTF8" "-O" "UTF8")) ; optional
