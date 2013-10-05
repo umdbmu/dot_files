5af89f8ce8e0756240dfe9f81f8eb4f817b0fc0f
umdbmu <umdlbmu@gmail.com>
2013-10-05 11:23:45 +0900

    add files

---
+++ emacs.d/conf/init-tag.el
@@ -0,0 +1,31 @@
+(require 'gtags)
+(require 'helm-gtags)
+(setq gtags-suggested-key-mapping t)
+;; 関数の定義元へ移動
+(global-set-key (kbd "C-c t") 'helm-gtags-find-tag)
+;;関数の参照元へ移動
+(global-set-key (kbd "C-c r") 'helm-gtags-find-rtag)
+;; タグで解析されているファイルへ移動
+(global-set-key (kbd "C-c p") 'helm-gtags-find-file)
+;; 変数の参照元と定義元へ移動
+(global-set-key (kbd "C-c s") 'helm-gtags-find-symbol)
+;; 現在のソースコード内で定義されている関数の一覧
+(global-set-key (kbd "C-c l") 'helm-gtags-parse-file)
+;;移動前の場所へ戻る
+(global-set-key (kbd "C-c a") 'helm-gtags-pop-stack)
+;; GTAGSを自動更新する
+(defun update-gtags (&optional prefix)
+  (interactive "P")
+  (let ((rootdir (gtags-get-rootpath))
+	(args (if prefix "-v" "-iv")))
+    (when rootdir
+      (let* ((default-directory rootdir)
+	     (buffer (get-buffer-create "*update GTAGS*")))
+	(save-excursion
+	  (set-buffer buffer)
+	  (erase-buffer)
+	  (let ((result (process-file "gtags" nil buffer nil args)))
+	    (if (= 0 result)
+		(message "GTAGS successfully updated.")
+	      (message "update GTAGS error with exit status %d" result))))))))
+(add-hook 'after-save-hook 'update-gtags)
