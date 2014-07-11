3cf26ae2d1186bdb2282c6fb77fd6558e261aa06
umdbmu <umdbmu@gmail.com>
2014-07-12 01:14:06 +0900

    tabbarの追加

--- emacs.d/conf/init-tabbar.el
+++ emacs.d/conf/init-tabbar.el
@@ -1,7 +1,6 @@
 ;; タブ上でマウスホイール操作無効
 (tabbar-mwheel-mode -1)
 (mouse-wheel-mode -1)
-(tabbar-buffer-home-button -1)
 
 (tabbar-mode 1)
 
@@ -16,96 +15,33 @@
                  (cons "" nil))))
 
 ;; タブの長さ
-(setq tabbar-separator '(1.5))
+(setq tabbar-separator '(0.8))
 
 ;; 外観変更
 (set-face-attribute
  'tabbar-default nil
- :family "MarkDown"
- :background "darkgreen"
- :foreground "red"
- :height 1.0)
-(set-face-attribute
- 'variable-pitch nil
- :family "Minion Pro"
- :background "red"
+ :background "brightblack"
+ ;; :foreground "red"
  :height 1.0)
 (set-face-attribute
  'tabbar-unselected nil
- :background "darkgreen"
- :foreground "black"
+ :background "brightblack"
+ :foreground "magenta"
+ :height 0.6
  :box nil)
 (set-face-attribute
  'tabbar-selected nil
- :background "darkgreen"
- :foreground "black"
+ :background "brightblue"
+ :foreground "white"
  :box t)
-(set-face-attribute
- 'tabbar-button nil
- :box nil)
+;; (set-face-attribute
+;;  'tabbar-button nil
+;;  :box nil)
 (set-face-attribute
  'tabbar-separator nil
  :height 1.3)
 
-;; タブに表示させるバッファの設定
-(defvar my-tabbar-displayed-buffers
-  '("*scratch*" "*Messages*" "*Backtrace*" "*Colors*" "*Faces*" "*vc-")
-  "*Regexps matches buffer names always included tabs.")
-
-(defun my-tabbar-buffer-list ()
-  "Return the list of buffers to show in tabs.
-Exclude buffers whose name starts with a space or an asterisk.
-The current buffer and buffers matches `my-tabbar-displayed-buffers'
-are always included."
-  (let* ((hides (list ?\  ?\*))
-         (re (regexp-opt my-tabbar-displayed-buffers))
-         (cur-buf (current-buffer))
-         (tabs (delq nil
-                     (mapcar (lambda (buf)
-                               (let ((name (buffer-name buf)))
-                                 (when (or (string-match re name)
-                                           (not (memq (aref name 0) hides)))
-                                   buf)))
-                             (buffer-list)))))
-    ;; Always include the current buffer.
-    (if (memq cur-buf tabs)
-        tabs
-      (cons cur-buf tabs))))
-
-(setq tabbar-buffer-list-function 'my-tabbar-buffer-list)
-
 ;;タブ切り替えのキーバインド
-(global-set-key (kbd "C-<tab>") 'tabbar-forward-tab)
-(global-set-key (kbd "C-S-<tab>") 'tabbar-backward-tab)
+(global-set-key "\M-]" 'tabbar-forward)
+(global-set-key "\M-[" 'tabbar-backward)
 
-;; タブ上をマウス中クリックで kill-buffer
-(defun my-tabbar-buffer-help-on-tab (tab)
-  "Return the help string shown when mouse is onto TAB."
-  (if tabbar--buffer-show-groups
-      (let* ((tabset (tabbar-tab-tabset tab))
-             (tab (tabbar-selected-tab tabset)))
-        (format "mouse-1: switch to buffer %S in group [%s]"
-                (buffer-name (tabbar-tab-value tab)) tabset))
-    (format "\
-mouse-1: switch to buffer %S\n\
-mouse-2: kill this buffer\n\
-mouse-3: delete other windows"
-            (buffer-name (tabbar-tab-value tab)))))
-
-(defun my-tabbar-buffer-select-tab (event tab)
-  "On mouse EVENT, select TAB."
-  (let ((mouse-button (event-basic-type event))
-        (buffer (tabbar-tab-value tab)))
-    (cond
-     ((eq mouse-button 'mouse-2)
-      (with-current-buffer buffer
-        (kill-buffer)))
-     ((eq mouse-button 'mouse-3)
-      (delete-other-windows))
-     (t
-      (switch-to-buffer buffer)))
-    ;; Don't show groups.
-    (tabbar-buffer-show-groups nil)))
-
-(setq tabbar-help-on-tab-function 'my-tabbar-buffer-help-on-tab)
-(setq tabbar-select-tab-function 'my-tabbar-buffer-select-tab)
