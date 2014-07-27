2a28e5183691c9ab26266bc978de9bb2afd32639
umdbmu <umdlbmu@gmail.com>
2014-07-27 14:42:07 +0000

    org-captureの設定追加

--- emacs.d/conf/init-lang.el
+++ emacs.d/conf/init-lang.el
@@ -1,15 +1,15 @@
 ;;;;;;;
 ;; c ;;
 ;;;;;;;
-;(load "init-cc")
+;; (load "init-cc")
 
  ;;;;;;;;;;;;;;
- ;; Markdown ;;
+;; Markdown ;;
  ;;;;;;;;;;;;;;
 ;; .mkファイルをmarkdown-modeで開く
 (setq auto-mode-alist
       (cons '("\\.mk?d$" .markdown-mode) auto-mode-alist))
-;(add-hook 'markdown-mode-hook 'flyspell-mode)
+;; (add-hook 'markdown-mode-hook 'flyspell-mode)
 ;; Marked でmarkdownファイルをプレビューする (Mac OSX時)
 (when (eq system-type 'darwin)
   (defun markdown-preview-file ()
@@ -17,7 +17,7 @@
     (interactive)
     (shell-command
      (format "open -a /Applications/Marked.app %s"
-	     (shell-quote-argument (buffer-file-name))))
+     (shell-quote-argument (buffer-file-name))))
     )
   (global-set-key "\C-cm" 'markdown-preview-file))
 
@@ -29,11 +29,63 @@
 ;; DONEの時刻を記録
 (setq org-log-done 'time)
 (define-key global-map "\C-ca" 'org-agenda)
-(define-key global-map "\C-cr" 'org-remember)
 ;; 見出しの余分な*を消す
 (setq org-hide-leading-stars t)
-(setq org-default-notes-file (quote "/GTD/notes.org"))
+(setq org-default-notes-file (concat "/GTD/note.org"))
 (define-key global-map "\C-cc" 'org-capture)
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "/GTD/inbox.org" "Inbox") "* TODO %?\n %i\n %a")
+	("h" "Hirameki" entry (file+headline "/GTD/inbox.org" "Hirameki") "* HIRAMEKi %?\n %i\n %a")))
+
+;;;;;;;;;;;
+;; YaTeX ;;
+;;;;;;;;;;;
+;; texファイルをyatexモードで開く
+(setq auto-mode-alist
+      (append '(("\\.tex$" . yatex-mode)) auto-mode-alist))
+(autoload 'yatex-mode "yatex" "Yet Another LaTeX mode" t)
+(setq YaTeX-inhibit-prefix-letter t)
+
+;;;;;;;;
+;; Go ;;
+;;;;;;;;
+;; go言語の設定をする
+(load "init-go")
+
+;;;;;;;;;;;;
+;; python ;;
+;;;;;;;;;;;;
+;; pythonの自動補完
+;; (require 'ac-python)
+;; (autoload 'python-mode "python-mode" "Python editing mode." t)
+
+;;;;;;;;;;
+;; Perl ;;
+;;;;;;;;;;
+;; perlの設定をする
+;; (load "init-perl")
+
+;;;;;;;
+;; R ;;
+;;;;;;;
+;; (load "init-R")
+
+;;;;;;;;;;
+;; HTML ;;
+;;;;;;;;;;
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+
+;;;;;;;;;;;;;;;;
+;; javascript ;;
+;;;;;;;;;;;;;;;;
+(require 'js2-mode)
+(setq js2-mode-hook
+      '(lambda()
+	 (setq js2-basic-offset 2)
+	 (setq tab-width 4)))
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 ;;;;;;;;;;;
 ;; YaTeX ;;
 ;;;;;;;;;;;
@@ -61,11 +113,6 @@
 ;;;;;;;;;;
 ;; perlの設定をする
 ; (load "init-perl")
-
-;;;;;;;
-;; R ;;
-;;;;;;;
-; (load "init-R")
 
 ;;;;;;;;;;
 ;; HTML ;;
