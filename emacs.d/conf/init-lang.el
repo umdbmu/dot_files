c6cf4e6d7a214dcc504a9206c54772200b420685
umdbmu <umdlbmu@gmail.com>
2016-02-25 10:37:03 +0900

    org-agendaビューにカスタムレイアウトを追加

--- emacs.d/conf/init-lang.el
+++ emacs.d/conf/init-lang.el
@@ -70,6 +70,21 @@
 (advice-add 'org-clocktable-indent-string :override #'my-org-clocktable-indent-string)
 (load "init-plantuml")
 
+(add-to-list 'org-agenda-custom-commands
+             '("D" agenda ""
+               ( ;; 1日分だけ表示する
+                (org-agenda-span 1)
+                ;; agenda各行の行頭のスペースをなくす
+                (org-agenda-prefix-format '((agenda . "%?-12t% s")))
+                ;; グリッドを表示しない
+                (org-agenda-use-time-grid nil)
+                ;; clockを表示する
+                (org-agenda-start-with-log-mode t)
+                (org-agenda-show-log 'clockcheck)
+                ;; clockの総計を表でまとめる
+                (org-agenda-start-with-clockreport-mode t)
+                (org-agenda-clockreport-mode t))))
+
 ;;;;;;;;;;;
 ;; YaTeX ;;
 ;;;;;;;;;;;
